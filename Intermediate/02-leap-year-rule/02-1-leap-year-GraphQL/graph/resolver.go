fb1cdc7d7707aef91e355e7882c6d12dab1a545e
Ivan Markovski <sagitariusim@gmail.com>
2024-01-10 01:26:46 +0100

    Task Intermediate 02-leap-year-rule-GraphQL

---
+++ Intermediate/02-leap-year-rule/02-1-leap-year-GraphQL/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/mkdtemplar/SimpleProblems/Intermediate/02-1-leap-year-GraphQL/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	LEAPYEAR model.LeapYears
+}
