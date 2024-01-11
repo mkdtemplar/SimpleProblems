2fd501e9735c911a9b39cfa88a68cad8c434ea60
Ivan Markovski <sagitariusim@gmail.com>
2024-01-11 15:22:16 +0100

    Task Intermediate 02-leap-year-rule-GraphQL

--- ListStrings/04-odd-positions-in-list/test/find-odd-position_test.go
+++ ListStrings/04-odd-positions-in-list/test/find-odd-position_test.go
@@ -7,13 +7,13 @@ import (
 	"github.com/mkdtemplar/SimpleProblems/ListStrings/04-odd-position-in-list/pkg"
 )
 
-func TestFindOddPosition(t *testing.T)  {
+func TestFindOddPosition(t *testing.T) {
 	inputSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	newSlice := pkg.NewSlice(inputSlice)
 
 	want := []int{2, 4, 6, 8, 10}
-	got:= newSlice.FindOddPosition()
+	got := newSlice.FindOddPosition()
 
 	if reflect.DeepEqual(want, got) == false {
 		t.Errorf("Test failed want %v, got %v", want, got)
--- ListStrings/05-running-total-of-a-list/test/sum_of_slice_test.go
+++ ListStrings/05-running-total-of-a-list/test/sum_of_slice_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestSumOfSlice(t *testing.T) {
-	inputSlice := []int{1,2,3,4,5,6,7,8,9,10}
+	inputSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	newSlice := pkg.NewSumSlice[int](inputSlice)
 
