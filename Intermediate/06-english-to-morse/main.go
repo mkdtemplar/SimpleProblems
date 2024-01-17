0a3c8fbb7def76aa030709e6c8f4be19fe299798
Ivan Markovski <sagitariusim@gmail.com>
2024-01-17 17:11:50 +0100

    Task Intermediate 0-6-english-to-morse

---
+++ Intermediate/06-english-to-morse/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func processTextFromFile(filename string) string {
+
+	text, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strings.ToUpper(string(text))
+}
+
+var morseCode = map[string]string{
+	"A": ".-",
+	"B": "-...",
+	"C": "-.-.",
+	"D": "-..",
+	"E": ".",
+	"F": "..-.",
+	"G": "--.",
+	"I": "..",
+	"J": ".---",
+	"K": "-.-",
+	"L": ".-..",
+	"M": "--",
+	"N": "-.",
+	"O": "---",
+	"P": ".--.",
+	"Q": "--.-",
+	"R": ".-.",
+	"S": "...",
+	"T": "-",
+	"U": "..-",
+	"V": "...-",
+	"W": ".--",
+	"X": "-..-",
+	"Y": "-.--",
+	"Z": "--..",
+	" ": "/",
+}
+
+func convertToMorseCode(text string) string {
+	builder := strings.Builder{}
+
+	for _, s := range text {
+		builder.WriteString(morseCode[string(s)])
+	}
+
+	return builder.String()
+}
+
+func main() {
+
+	text := processTextFromFile("text.txt")
+
+	morseCodeConvert := convertToMorseCode(text)
+	fmt.Println("Blank space separator /")
+
+	fmt.Println(morseCodeConvert)
+}
