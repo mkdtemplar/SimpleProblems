113f2807d82e4f717442c5d592a9566e61e43308
Ivan Markovski <sagitariusim@gmail.com>
2024-02-16 15:07:38 +0100

    task 03-intermediate/07 updated using channels

--- 03-Intermediate/07-longest-palindromic-substring/main.go
+++ 03-Intermediate/07-longest-palindromic-substring/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // Function to check if a string is a palindrome
 func isPalindrome(s string) bool {
@@ -15,36 +13,52 @@ func isPalindrome(s string) bool {
 	return true
 }
 
-func maxLengthPalindrome(s []string) string {
+func maxLengthPalindrome(s []string, strChan chan string) chan string {
+
 	var maxLength = len(s[0])
 	index := 0
-
-	for i, e := range s {
-		if len(e) > maxLength {
-			maxLength = len(e)
-			index = i
+	go func() {
+		for i, e := range s {
+			if len(e) > maxLength {
+				maxLength = len(e)
+				index = i
+			}
 		}
-	}
-	return s[index]
+		strChan <- s[index]
+	}()
+
+	return strChan
+
 }
 
 // Function to find the longest palindromic substring of a given string
-func longestPalindrome(s string) string {
+func longestPalindrome(s string, strChan chan []string) chan []string {
 	var palindromes []string
 
-	for i := 0; i < len(s); i++ {
-		for j := i; j < len(s); j++ {
-			if isPalindrome(s[i:j+1]) && len(s[i:j+1]) > 1 {
-				palindromes = append(palindromes, s[i:j+1])
+	go func() {
+		for i := 0; i < len(s); i++ {
+			for j := i; j < len(s); j++ {
+				if isPalindrome(s[i:j+1]) && len(s[i:j+1]) > 1 {
+					palindromes = append(palindromes, s[i:j+1])
+				}
 			}
 		}
-	}
+		strChan <- palindromes
+	}()
 
 	// Return the longest palindromic substring
-	return maxLengthPalindrome(palindromes)
+	return strChan
+
 }
 
 func main() {
-	fmt.Println(longestPalindrome("asdfasdf1234321asd32"))
+
+	strChan := make(chan []string)
+	maxStringChan := make(chan string)
+
+	palindromes := <-longestPalindrome("asdfasdf1234321asd32", strChan)
+
+	maxSubString := <-maxLengthPalindrome(palindromes, maxStringChan)
+	fmt.Println(maxSubString)
 
 }
