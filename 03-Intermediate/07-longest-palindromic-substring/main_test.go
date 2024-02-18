e861ed425315a3d2862d3d1768d3d4c2b5c14c69
Ivan Markovski <sagitariusim@gmail.com>
2024-02-18 16:19:50 +0100

    Task Intermediate 07 updated with channels and updated tests

--- 03-Intermediate/07-longest-palindromic-substring/main_test.go
+++ 03-Intermediate/07-longest-palindromic-substring/main_test.go
@@ -56,7 +56,7 @@ func Test_longestPalindrome(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			if got := <-longestPalindrome(tt.args.s, tt.args.strChan); !reflect.DeepEqual(got, tt.want) {
+			if got := longestPalindrome(tt.args.s, tt.args.strChan); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
 			}
 		})
